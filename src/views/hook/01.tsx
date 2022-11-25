8c74af896cccda2fcf602afb051e19079219d986
zhj <zhang723561@163.com>
2022-11-25 16:55:45 +0800

    组件

---
+++ src/views/hook/01.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+export  const DemoState = (props:any) => {
+    /* number为此时state读取值 ，setNumber为派发更新的函数 */
+    let [number, setNumber] = React.useState(0) /* 0为初始值 */
+    return (<div>
+        <span>{ number }</span>
+        <button onClick={ ()=> {
+            setNumber(number+1)
+            console.log(number) /* 这里的number是不能够即使改变的  */
+        } } >+1</button>
+    </div>)
+}
--- src/views/modal.tsx
+++ src/views/modal.tsx
@@ -1,10 +1,11 @@
 
+import {DemoState} from './hook/01'
 export default function modal() {
     const name = '123'
 
-
     return (
         <div className="modal-wrapper">
+            {DemoState(0)}
             <div className="modal">
                 <div className="modal-title">这是modal标题</div>
                 <div className="modal-content">这是modal内容{name}</div>
